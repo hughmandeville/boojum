c31082795f048a3c7cf4dbec2a3a96ee28d1ebd0
Hubert Mandeville <hubert.mandeville@nytimes.com>
2015-11-24 21:31:33 -0500

    Initial checkin.

---
+++ install.sh
@@ -0,0 +1,5 @@
+scp index.html escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/
+scp style.css escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/
+#scp images/bg_seth_2000x2667.jpg escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/images/
+#scp images/bg_seth_100x100.png escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/images/
+
