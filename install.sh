da066de2690e8b302a9dd2cafc9b0a951736aefc
Hubert Mandeville <hubert.mandeville@nytimes.com>
2015-11-26 17:15:05 -0500

    Updated.

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 #scp -P 2222 .htaccess mouse2277@theboojum.org:/home1/mouse2277/public_html/
 scp -P 2222 code.js mouse2277@theboojum.org:/home1/mouse2277/public_html/
-scp -P 2222 index.html mouse2277@theboojum.org:/home1/mouse2277/public_html/
+#scp -P 2222 index.html mouse2277@theboojum.org:/home1/mouse2277/public_html/
 #scp -P 2222 missing.html mouse2277@theboojum.org:/home1/mouse2277/public_html/
 scp -P 2222 style.css mouse2277@theboojum.org:/home1/mouse2277/public_html/
-#scp -P 2222 images/favicon.png mouse2277@theboojum.org:/home1/mouse2277/public_html/
+#scp -P 2222 images/favicon.png mouse2277@theboojum.org:/home1/mouse2277/public_html/images/
 
