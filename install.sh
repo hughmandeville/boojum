e642d7bcb8666b69a10e5d8468835a1bcbb568d1
Hubert Mandeville <hubert.mandeville@nytimes.com>
2015-11-26 11:21:30 -0500

    Lines to install images.

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ scp index.html escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/
 scp style.css escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/
 #scp images/bg_seth_2000x2667.jpg escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/images/
 #scp images/bg_seth_100x100.png escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/images/
+#scp images/logo_starshipsofa_330x150.png escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/images/
+#scp images/logo_tales_to_terrify_330x150.png escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/images/
+#scp images/logo_far-fetched_fables_330x150.png escalona@medianoche.us:/home/escalona/medianoche.us/hugh/boojum/images/
 
