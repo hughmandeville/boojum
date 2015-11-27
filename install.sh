1d7cd33164a4104c05cfcf672d4d16f74281486e
Hubert Mandeville <hubert.mandeville@nytimes.com>
2015-11-27 16:17:55 -0500

    Copy style.

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@
 scp -P 2222 index.html mouse2277@theboojum.org:/home1/mouse2277/public_html/
 #scp -P 2222 missing.html mouse2277@theboojum.org:/home1/mouse2277/public_html/
 #scp -P 2222 robots.txt mouse2277@theboojum.org:/home1/mouse2277/public_html/
-#scp -P 2222 style.css mouse2277@theboojum.org:/home1/mouse2277/public_html/
+scp -P 2222 style.css mouse2277@theboojum.org:/home1/mouse2277/public_html/
 #scp -P 2222 images/favicon.png mouse2277@theboojum.org:/home1/mouse2277/public_html/images/
 
