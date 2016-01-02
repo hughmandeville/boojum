4a921a2eaba1d850a0bea1c36a06ddc17bf6bc6c
Hugh Mandeville <hubert.mandeville@nytimes.com>
2016-01-01 23:36:51 -0500

    Added podcast far-fetched fables image.

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ scp -P 2222 index.html mouse2277@theboojum.org:/home1/mouse2277/public_html/
 #scp -P 2222 robots.txt mouse2277@theboojum.org:/home1/mouse2277/public_html/
 scp -P 2222 style.css mouse2277@theboojum.org:/home1/mouse2277/public_html/
 #scp -P 2222 images/favicon.png mouse2277@theboojum.org:/home1/mouse2277/public_html/images/
+#scp -P 2222 images/podcast_far-fetched_fables_87.png mouse2277@theboojum.org:/home1/mouse2277/public_html/images/
 
