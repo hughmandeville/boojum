71cd07b17aa93f228edf98a44f1a284c33873ee6
Hugh Mandeville <hubert.mandeville@nytimes.com>
2016-02-04 23:20:36 -0500

    Added logo.

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 scp -P 2222 index.html mouse2277@theboojum.org:/home1/mouse2277/public_html/
 #scp -P 2222 missing.html mouse2277@theboojum.org:/home1/mouse2277/public_html/
 #scp -P 2222 robots.txt mouse2277@theboojum.org:/home1/mouse2277/public_html/
-scp -P 2222 style.css mouse2277@theboojum.org:/home1/mouse2277/public_html/
+#scp -P 2222 style.css mouse2277@theboojum.org:/home1/mouse2277/public_html/
 #scp -P 2222 images/favicon.png mouse2277@theboojum.org:/home1/mouse2277/public_html/images/
+#scp -P 2222 images/logo_boojum.png mouse2277@theboojum.org:/home1/mouse2277/public_html/images/
 #scp -P 2222 images/podcast_far-fetched_fables_87.png mouse2277@theboojum.org:/home1/mouse2277/public_html/images/
 
